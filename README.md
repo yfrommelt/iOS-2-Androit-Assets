727c7528691686d67f2a7d3d8f85ade9d8670479
Yoann <yoann@frommelt.fr>
2014-07-02 17:41:06 +0200

    add resize

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-iOS-2-Androit-Assets
-====================
+#iOS-2-Androit-Assets
 
+##DEscription
 Rename iOS style Assets to Android style Drawabel
+
+##Before Start
+Need http://www.imagemagick.org/script/binary-releases.php
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var morph = require('morph')
 var mkdirp = require('mkdirp');
 var path = require('path');
 var fs = require('fs');
+var im = require('imagemagick');
 
 window.ondragover = window.ondrop = function(e) {
 	e.preventDefault();
@@ -22,29 +23,48 @@ dropZone.ondragleave = function(e) {
 	return false;
 }
 
-var mdip = 'drawable-mdip';
-var xhdip = 'drawable-xhdip';
-
+var density = {
+	'ldip' : 0.25,
+	'mdip' : 0.5,
+	'hdip' : 0.75,
+	'xhdip' : 1
+};
 
 dropZone.ondrop = function(e) {
 	e.preventDefault();
 
-	for (var i = 0; i < e.dataTransfer.files.length; i++) {
+	var source_dirname = path.dirname(e.dataTransfer.files[0].path);
+	for (var resolution in density) {
+		mkdirp(source_dirname + path.sep + 'drawable-' + resolution, function(err) {});
+	}
+
+	var nb_files = e.dataTransfer.files.length;
+
+	for (var i = 0; i < nb_files; i++) {
+		var converted = 0;
 		var source_file = e.dataTransfer.files[i].path;
 		var source_extension = path.extname(source_file);
 		var source_basename = path.basename(source_file, source_extension);
-		var source_dirname = path.dirname(source_file);
 		var target_basename = morph.toSnake(source_basename.replace('@2x', ''));
 
-		var target_res_dirname = (source_basename.indexOf('@2x') > 0) ? xhdip : mdip;
-		var target_dirname = source_dirname + path.sep + target_res_dirname;
+		if (source_basename.indexOf('@2x') > 0) {
+			for (var resolution in density) {
+				var target_dirname = source_dirname + path.sep + 'drawable-' + resolution;
+				var target_file = target_dirname + path.sep + target_basename + source_extension;
 
-		mkdirp(target_dirname, function(err) {});
+				im.convert([source_file, '-resize', (density[resolution]*100)+'%', target_file], function(err, stdout) {
+					if (err) throw err;
+					// converted++;
+					// if (converted == (Object.keys(density).length * nb_files)) {
+					// 	this.className = "";
+					// 	dropZone.innerHTML = "Travail terminé";
+					// }
+				});
+			}
+		}
 
-
-		var target_file = target_dirname + path.sep + target_basename + source_extension;
-		copyFile(source_file, target_file, function(err) {});
-
+		this.className = "";
+		dropZone.innerHTML = "Travail terminé";
 	}
 }
 
