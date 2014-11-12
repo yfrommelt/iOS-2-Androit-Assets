ab30cc5040cf0ae3d2ebcc885ac7a7137ba7f0fc
Yoann <yoann@frommelt.fr>
2014-11-12 14:20:52 +0100

    Add support for extra hight resolution
    • iOS @3x will be take first
    • add android xxhdpi and xxxhdpi folder

--- app.js
+++ app.js
@@ -12,58 +12,89 @@ window.ondragover = window.ondrop = function(e) {
 var dropZone = document.querySelector('#drop');
 
 dropZone.ondragover = function(e) {
-	this.className = "hover";
-	this.innerHTML = "Drop the files";
+	this.className = 'hover';
+	this.innerHTML = 'Drop the files';
 	return false;
 };
 
 dropZone.ondragleave = function(e) {
-	this.className = "";
-	this.innerHTML = "Drop your iOS assets here";
+	this.className = '';
+	this.innerHTML = 'Drop your iOS assets here';
 	return false;
 };
 
 var density = {
-	'mdip' : 50,
-	'hdip' : 75,
-	'xhdip' : 100,
-	'xxhdip' : 112.5,
+	'mdpi': 1,
+	'hdpi': 1.5,
+	'xhdpi': 2,
+	'xxhdpi': 3,
+	'xxxhdpi': 4,
 };
+
+
+var matching = {
+	'@2x': 'xhdpi',
+	'@3x': 'xxhdpi',
+	'@4x': 'xxxhdpi', //prevision 2014-11-06
+}
+
+var reg = /@(\d)x/i;
 
 dropZone.ondrop = function(e) {
 	e.preventDefault();
 
-	var source_dirname = path.dirname(e.dataTransfer.files[0].path);
+	var files = Object.keys(e.dataTransfer.files).map(function(k) { return e.dataTransfer.files[k] });
+	var files_complete = new Array();
+
+	var source_dirname = path.dirname(files[0].path);
 	for (var resolution in density) {
 		mkdirp(source_dirname + path.sep + 'drawable-' + resolution, function(err) {});
 	}
+	
+	files.sort(function(a, b) {
+		if (typeof a != 'object' || typeof b != 'object') {
+			return;
+		}
+		return (a.name.match(reg)[1] < b.name.match(reg)[1]);
+	});
 
-	var nb_files = e.dataTransfer.files.length;
-
-	for (var i = 0; i < nb_files; i++) {
+	for (var i = 0; i < files.length; i++) {
 		var converted = 0;
-		var source_file = e.dataTransfer.files[i].path;
+		var source_file = files[i].path;
 		var source_extension = path.extname(source_file);
 		var source_basename = path.basename(source_file, source_extension);
-		var target_basename = morph.toSnake(source_basename.replace('@2x', ''));
+		var target_basename = morph.toSnake(source_basename.replace(reg, ''));
 
-		if (source_basename.indexOf('@2x') > 0) {
+		var maches = source_basename.match(reg);
+		if (maches && maches.length > 0) {
+			console.log(maches[0]);
+			var ratio = (1/density[matching[maches[0]]]);
 			for (var resolution in density) {
+
+				var resize = ((density[resolution]*ratio)*100);
+
 				var target_dirname = source_dirname + path.sep + 'drawable-' + resolution;
 				var target_file = target_dirname + path.sep + target_basename + source_extension;
 
-				im.convert([source_file, '-resize', (density[resolution])+'%', target_file], function(err, stdout) {
-					if (err) throw err;
-					// converted++;
-					// if (converted == (Object.keys(density).length * nb_files)) {
-					// 	this.className = "";
-					// 	dropZone.innerHTML = "Travail terminé";
-					// }
-				});
+
+				// console.log(target_file);
+				// console.log(files_complete.indexOf(target_file), resize, 100 > resize);
+				
+				if (-1 === files_complete.indexOf(target_file)
+					|| (-1 !== files_complete.indexOf(target_file) && 100 > resize)) {
+
+					im.convert([source_file, '-resize', resize+'%', target_file], function(err, stdout) {
+						if (err) throw err;
+					});
+
+				}
+
+				files_complete.push(target_file);
+
 			}
 		}
 
-		this.className = "";
-		dropZone.innerHTML = "Travail terminé";
+		this.className = '';
+		dropZone.innerHTML = 'Resize finished !';
 	}
 };
